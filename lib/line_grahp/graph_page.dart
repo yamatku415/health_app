ae009c8fee74e8d97f3af23fc80dcc85138e49a7
takum <takumi1129@hotmail.com>
2021-10-20 22:30:19 +0900

    グラフを二本反映させた値が違くても一つのグラフ内で表示させられている。

--- lib/line_grahp/graph_page.dart
+++ lib/line_grahp/graph_page.dart
@@ -1,109 +1,61 @@
+import 'dart:math';
+
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
-import 'package:health_app/line_grahp/weight_data.dart';
 
-class GraphPage extends StatefulWidget {
-  GraphPage();
+class GraphPage extends StatelessWidget {
+  const GraphPage({Key? key}) : super(key: key);
 
-  @override
-  _GraphPageState createState() => _GraphPageState();
-}
-
-///表示するページ
-class _GraphPageState extends State<GraphPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: FloatingActionButton(
-          child: Text('戻る'),
-          onPressed: () {
-            // 1つ前に戻る
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Stack(children: <Widget>[
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('体重グラフ'),
-              Container(
-                height: 500,
-                //グラフ表示部分
-                child: charts.TimeSeriesChart(
-                  _createWeightData(nowWeighList),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('体重グラフ'),
-              Container(
-                height: 500,
-                //グラフ表示部分
-                child: charts.TimeSeriesChart(
-                  _anCreateWeightData(anWeightList),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ]),
+      appBar: AppBar(title: Text("chart")),
+      body: Column(children: [Container(height: 240, child: _simpleLine())]),
     );
   }
 
-//WeightDataのリストを作成。好きな日付と体重入れよう
-  //z ここをmain.dartから引っ張ってきた数字を代入して表示
-  final nowWeighList = <NowWeightData>[
-    NowWeightData(DateTime(2021, 10, 1), 80),
-    NowWeightData(DateTime(2021, 10, 7), 77),
-    NowWeightData(DateTime(2021, 10, 14), 73),
-    NowWeightData(DateTime(2021, 10, 21), 69),
-    NowWeightData(DateTime(2021, 10, 28), 66),
-    NowWeightData(DateTime(2021, 11, 7), 65),
-  ];
+  Widget _simpleLine() {
+    var random = Random();
 
-  final anWeightList = <AnWeightData>[
-    AnWeightData(DateTime(2021, 10, 1), 70),
-    AnWeightData(DateTime(2021, 10, 7), 67),
-    AnWeightData(DateTime(2021, 10, 14), 63),
-    AnWeightData(DateTime(2021, 10, 21), 59),
-    AnWeightData(DateTime(2021, 10, 28), 56),
-    AnWeightData(DateTime(2021, 11, 7), 55),
-  ];
-
-//上のリストからグラフに表示させるデータを生成
-  List<charts.Series<NowWeightData, DateTime>> _createWeightData(
-      List<NowWeightData> weightList) {
-    return [
-      charts.Series<NowWeightData, DateTime>(
-        id: 'Muscles',
-        data: weightList,
-        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-        domainFn: (NowWeightData nowWeightData, _) =>
-            nowWeightData.nowDate ?? DateTime(2021, 10, 1),
-        measureFn: (NowWeightData nowWeightData, _) => nowWeightData.nowWeight,
-      )
+    //0のところうを日にち、ランダムの所を体重　　　date１を理想体重
+    var data1 = [
+      LinearSales(0, random.nextInt(100)),
+      LinearSales(1, random.nextInt(100)),
+      LinearSales(2, random.nextInt(100)),
+      LinearSales(3, random.nextInt(100)),
     ];
-  }
-
-  List<charts.Series<AnWeightData, DateTime>> _anCreateWeightData(
-      List<AnWeightData> anWeightList) {
-    return [
-      charts.Series<AnWeightData, DateTime>(
-        id: 'AnMuscles',
-        data: anWeightList,
-        colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
-        domainFn: (AnWeightData anWeightData, _) =>
-            anWeightData.anData ?? DateTime(2021, 10, 1),
-        measureFn: (AnWeightData anWeightData, _) => anWeightData.anWeight,
-      )
+    //date2を今日の体重で追加していく
+    var data2 = [
+      LinearSales(0, random.nextInt(100)),
+      LinearSales(1, random.nextInt(100)),
     ];
+
+    var seriesList = [
+      charts.Series<LinearSales, int>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.ColorUtil.fromDartColor(Color(0xFF6300A1)),
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        //dashPatternFn: (_, __) => [8, 2, 4, 2],
+        data: data1,
+      ),
+      charts.Series<LinearSales, int>(
+        id: 'Use',
+        colorFn: (_, __) => charts.ColorUtil.fromDartColor(Color(0xFF13A331)),
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        //dashPatternFn: (_, __) => [8, 2, 4, 2],
+        data: data2,
+      ),
+    ];
+
+    return charts.LineChart(seriesList, animate: true);
   }
+}
+
+class LinearSales {
+  final int year;
+  final int sales;
+
+  LinearSales(this.year, this.sales);
 }
